b510f38c5f47b4a006de1fc5823b1e87e8a357f0
AntonyZuniga <106580994+AntonyZuniga@users.noreply.github.com>
2022-06-13 01:51:23 -0500

    Se agrega avance de 50%
    Se agrega lo necesario para el 50% requerido del proyecto, su header con identidad del proyecto, carrusel, pie de página y formulario.

--- public/index.php
+++ public/index.php
@@ -1,55 +1,249 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IDGSGamers</title>
+    <link rel="shortcut icon" href="../resources/img/logo.png" />
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../resources/css/navbar.css">
+    <link rel="stylesheet" href="../resources/css/carrusel.css">
+    <link rel="stylesheet" href="../resources/css/footer.css">
+    
+    <!----===== ICONOS ===== -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+    <!----===== BOOTSTRAP ===== -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet">
 
-define('LARAVEL_START', microtime(true));
+    <!----===== SCRIPT ===== -->
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+</head>
+<body>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                <img src="../resources/img/logo.png" alt="">
+                </span>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+                <div class="text logo-text">
+                    <span class="name">IDGSGamers</span>
+                    <span class="profession">amantes del gaming</span>
+                </div>
+            </div>
 
-require __DIR__.'/../vendor/autoload.php';
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+        <div class="menu-bar">
+            <div class="menu">
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+                <li class="search-box">
+                    <i class='bx bx-search icon'></i>
+                    <input type="text" placeholder="Search...">
+                </li>
 
-$kernel = $app->make(Kernel::class);
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fas fa-home icon'></i>
+                            <span class="text nav-text">Inicio</span>
+                        </a>
+                    </li>
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fab fa-xbox icon'></i>
+                            <span class="text nav-text">Xbox</span>
+                        </a>
+                    </li>
 
-$kernel->terminate($request, $response);
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fab fa-playstation icon'></i>
+                            <span class="text nav-text">Play Station</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fab fa-steam icon'></i>
+                            <span class="text nav-text">PC</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fas fa-gamepad icon'></i>
+                            <span class="text nav-text">Nintendo</span>
+                        </a>
+                    </li>
+
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="../resources/views/login.php">
+                        <i class='fas fa-user-circle icon'></i>
+                        <span class="text nav-text">Iniciar Sesion</span>
+                    </a>
+                </li>
+
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Modo Oscuro</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+                
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+
+        <div class="contenedor">
+          <div class="slider-contenedor">
+              <section class="contenido-slider">
+                  <div>
+                      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ipsa ipsam aliquam natus quidem
+                          sed. Quos cupiditate id delectus est.</h2>
+                      <a href="#">Contact us</a>
+                  </div>
+                  <img src="../resources/img/juegos.jpg" alt="">
+  
+              </section>
+              <section class="contenido-slider">
+                  <div>
+                      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ipsa ipsam aliquam natus quidem
+                          sed. Quos cupiditate id delectus est.</h2>
+                      <a href="#">Contact us</a>
+                  </div>
+                  <img src="../resources/img/xbox.jpg" alt="">
+  
+              </section>
+          <section class="contenido-slider">
+              <div>
+                <h1>Play Station</h1>
+                  <h2>PlayStation es una consola de videojuegos enfocada en los jugadores con un diseño impresionante que 
+                    cumple con lo que promete, ser un juego de sobremesa con exclusivos únicos.</h2>
+                  <a href="https://www.playstation.com/es-mx/">Sitio Oficial</a>
+              </div>
+              <img src="../resources/img/ps.jpg" alt="">
+  
+          </section>
+          <section class="contenido-slider">
+              <div>
+                  <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ipsa ipsam aliquam natus quidem
+                      sed. Quos cupiditate id delectus est.</h2>
+                  <a href="#">Contact us</a>
+              </div>
+              <img src="../resources/img/pc.jpg" alt="">
+  
+          </section>
+          <section class="contenido-slider">
+            <div>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ipsa ipsam aliquam natus quidem
+                    sed. Quos cupiditate id delectus est.</h2>
+                <a href="#">Contact us</a>
+            </div>
+            <img src="../resources/img/juegos.jpg" alt="">
+
+        </section>
+      </div>
+      </div>
+      <script src="../resources/js/carrusel.js"></script>
+<br><br><br><br><br><br>
+<hr/>
+<footer class="footer-distributed">
+
+        <div class="footer-left">
+  
+          <img class="logof" src="../resources/img/logo.png" alt="">
+  
+          <p class="footer-links">
+            <a href="index.php">Inicio</a>
+            ·
+            <a href="#">Nosotros</a>
+            ·
+            <a href="#">Acerca de la página</a>
+            
+          </p>
+  
+          <p class="footer-company-name">IDGSGamers © 2022</p>
+  
+          <div class="footer-icons">
+  
+            <a href="#"><i class='fab fa-facebook-square'></i></a>
+            <a href="#"><i class='fab fa-twitter'></i></i></a>
+            <a href="#"><i class='fab fa-github'></i></i></a>
+            <a href="#"><i class='fab fa-steam'></i></i></a>
+  
+          </div>
+  
+        </div>
+  
+        <div class="footer-right">
+  
+          <p>Contactanos</p>
+  
+          <form action="#" method="post">
+  
+            <input type="text" name="email" placeholder="Correo Electrónico">
+            <textarea name="message" placeholder="Mensaje"></textarea>
+            <button>Enviar</button>
+  
+          </form>
+  
+        </div>
+  
+ </footer>
+    </section>
+    
+
+    
+
+    <script>
+        const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Modo Luz";
+    }else{
+        modeText.innerText = "Modo Oscuro";
+        
+    }
+});
+    </script>
+</body>
+
+</html>
---
+++ resources/views/login.php
@@ -0,0 +1,285 @@
+<?php require_once '../php/authController.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IDGSGamers</title>
+    <link rel="shortcut icon" href="../img/logo.png" />
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/login.css">
+
+    <!----===== ICONOS ===== -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+    <!----===== BOOTSTRAP ===== -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet">
+
+    <!----===== SCRIPT ===== -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                <img src="../img/logo.png" alt="">
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">IDGSGamers</span>
+                    <span class="profession">amantes del gaming</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+
+                <li class="search-box">
+                    <i class='bx bx-search icon'></i>
+                    <input type="text" placeholder="Search...">
+                </li>
+
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="../../public/index.php">
+                            <i class='fas fa-home icon'></i>
+                            <span class="text nav-text">Inicio</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fab fa-xbox icon'></i>
+                            <span class="text nav-text">Xbox</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fab fa-playstation icon'></i>
+                            <span class="text nav-text">Play Station</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fab fa-steam icon'></i>
+                            <span class="text nav-text">PC</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='fas fa-gamepad icon'></i>
+                            <span class="text nav-text">Nintendo</span>
+                        </a>
+                    </li>
+
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <i class='fas fa-user-circle icon'></i>
+                        <span class="text nav-text">Iniciar Sesion</span>
+                    </a>
+                </li>
+
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Modo Oscuro</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+                
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+
+<br class="hide">
+<br class="hide">
+<br class="hide">
+
+    <div class="login-wrap">
+        <div class="login-html">
+            <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Iniciar Sesion</label>
+            <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Registrarse</label>
+            <div class="login-form">
+
+                <form action="sesion.php" method="POST">
+                <div class="sign-in-htm">
+
+                    <?php if(count($errors) > 0): ?>
+                    <div class="alert alert-danger">
+                        <?php foreach($errors as $error):?>
+                        <li><?php echo $error;?></li>
+                        <?php endforeach;?>
+                    </div>
+                    <?php endif;?>
+
+                    <div class="group">
+                        <label for="user" class="label">Usuario / Correo Electronico</label>
+                        <input id="user" name="user" type="text" class="input"  maxlength="100" >
+                    </div>
+                    <div class="group">
+                        <label for="pass" class="label">Contraseña</label>
+                        <input id="pass" name="pass" type="password" class="input" data-type="password" maxlength="25" >
+                    </div>
+                    <div class="group">
+                        <input id="check" type="checkbox" class="check" checked>
+                        <label for="check"><span class="icon"></span> Mantener sesion iniciada</label>
+                    </div>
+                    <div class="group">
+                        <input type="submit" name="login" class="button" value="Iniciar Sesion">
+                    </div>
+                    <div class="hr"></div>
+                    <div class="foot-lnk">
+                        <label for="tab-2">No Tienes Cuenta?</a>
+                    </div>
+                </div>
+                </form>
+
+                <form action="sesion.php" method="POST">
+                <div class="sign-up-htm">
+
+                    <?php if(count($errors) > 0): ?>
+                    <div class="alert alert-danger">
+                        <?php foreach($errors as $error):?>
+                        <li><?php echo $error;?></li>
+                        <?php endforeach;?>
+                    </div>
+                    <?php endif;?>
+                    
+                    <div class="group">
+                        <label for="usuario" class="label">Usuario</label>
+                        <input id="user" value="<?php echo $username; ?>" name="user" type="text" class="input" pattern="[a-zA-Z0-9]+" minlength="5" maxlength="25" >
+                    </div>
+                    <div class="group">
+                        <label for="email" class="label">Correo Electronico</label>
+                        <input id="email" value="<?php echo $email; ?>" name="email" type="text" class="input" maxlength="100">
+                    </div>
+                    <div class="group">
+                        <label for="pass1" class="label">Contraseña</label>
+                        <input id="pass1" name="pass" type="password" class="input" data-type="password" minlength="8" maxlength="25">
+                    </div>
+                    <div class="group">
+                        <label for="pass2" class="label">Confirmar Contraseña</label>
+                        <input id="pass2" name="passConf" type="password" class="input" data-type="password">
+                    </div>
+                    <div class="group">
+                        <input type="submit" name="register" class="button" value="Registrarse">
+                    </div>
+                    <div class="hr"></div>
+                    <div class="foot-lnk">
+                        <label for="tab-1">Ya Tienes Cuenta?</a>
+                    </div>
+                </div>
+
+                </form>
+            </div>
+        </div>
+    </div>    
+
+<br><br><br><br>
+<hr/>
+<footer class="footer-distributed">
+
+        <div class="footer-left">
+  
+          <img class="logof" src="../img/logo.png" alt="">
+  
+          <p class="footer-links">
+            <a href="../../public/index.php">Inicio</a>
+            ·
+            <a href="#">Nosotros</a>
+            ·
+            <a href="#">Acerca de la página</a>
+            
+          </p>
+  
+          <p class="footer-company-name">IDGSGamers © 2022</p>
+  
+          <div class="footer-icons">
+  
+            <a href="#"><i class='fab fa-facebook-square'></i></a>
+            <a href="#"><i class='fab fa-twitter'></i></i></a>
+            <a href="#"><i class='fab fa-github'></i></i></a>
+            <a href="#"><i class='fab fa-steam'></i></i></a>
+  
+          </div>
+  
+        </div>
+  
+        <div class="footer-right">
+  
+          <p>Contactanos</p>
+  
+          <form action="#" method="post">
+  
+            <input type="text" name="email" placeholder="Correo Electrónico">
+            <textarea name="message" placeholder="Mensaje"></textarea>
+            <button>Enviar</button>
+  
+          </form>
+  
+        </div>
+  
+ </footer>
+    </section>
+    
+
+    
+
+    <script>
+        const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Modo Luz";
+    }else{
+        modeText.innerText = "Modo Oscuro";
+        
+    }
+});
+    </script>
+</body>
+
+</html>
