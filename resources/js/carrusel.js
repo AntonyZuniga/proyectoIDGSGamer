fd13565c201b0ac7fb55a043f13aadb9846728f1
SoerAlberto <guerreroortiz10@gmail.com>
2022-08-03 01:58:13 -0500

    VERSION 1.0

--- resources/js/carrusel.js
+++ resources/js/carrusel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let slider = document.querySelector(".slider-contenedor")
 let sliderIndividual = document.querySelectorAll(".contenido-slider")
 let contador = 1;
@@ -27,34 +26,4 @@ function slides(){
             contador=1;
         },1500)
     }
-=======
-let slider = document.querySelector(".slider-contenedor")
-let sliderIndividual = document.querySelectorAll(".contenido-slider")
-let contador = 1;
-let width = sliderIndividual[0].clientWidth;
-let intervalo = 6000;
-
-window.addEventListener("resize", function(){
-    width = sliderIndividual[0].clientWidth;
-})
-
-setInterval(function(){
-    slides();
-},intervalo);
-
-
-
-function slides(){
-    slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform .8s";
-    contador++;
-
-    if(contador == sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            contador=1;
-        },1500)
-    }
->>>>>>> d114589032201eddb825151d9e0bc8f77c84c0e2
 }
